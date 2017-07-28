1ef279917b5178be2bc3502b257a95520247f474
Mike Chu <mike@chu.ms>
2017-07-28 11:42:13 -0600

    App: Moved to ES2017 structure; setting up for iteration.

--- src/app.js
+++ src/app.js
@@ -6,59 +6,94 @@ import QtiCorrector from './QtiCorrector';
 import ManifestCorrector from './ManifestCorrector';
 import FileHandler from './FileHandler';
 
-console.log("Starting...");
+class App {
+    static async process(assessmentId, guid) {
+        console.log(`Starting to process ${assessmentId} in Sakai course ${guid}...`);
+        const baseFolder = 'c:\\Users\\mike\\Downloads';
+        const urlToDownloadFrom = `https://study.ashworthcollege.edu/samigo-app/servlet/DownloadCP?&assessmentId=${assessmentId}`;
+        const pathToSaveZipTo = `${baseFolder}\\${guid}\\Sakai-${assessmentId}.zip`;
+        const pathToUnzipTo = `${baseFolder}\\${guid}\\${assessmentId}`;
+        const qtiXmlFile = `${pathToUnzipTo}\\exportAssessment.xml`;
+        const manifestXmlFile = `${pathToUnzipTo}\\imsmanifest.xml`;
 
-async function start(assessmentId, guid) {
-    const urlToDownloadFrom = `https://study.ashworthcollege.edu/samigo-app/servlet/DownloadCP?&assessmentId=${assessmentId}`;
-    const pathToSaveZipTo = `c:\\Users\\mike\\Downloads\\${guid}\\${assessmentId}.zip`;
-    const folderWithUnzippedContent = 'c:\\Users\\mike\\Downloads\\unzipped';
-    const qtiXmlFile = `${folderWithUnzippedContent}\\exportAssessment.xml`;
-    const manifestXmlFile = `${folderWithUnzippedContent}\\imsmanifest.xml`;
+        await Downloader.downloadPackage(urlToDownloadFrom, pathToSaveZipTo);
+        await Archiver.extractContentPackage(pathToSaveZipTo, pathToUnzipTo);
 
-    await Downloader.downloadPackage(urlToDownloadFrom, pathToSaveZipTo);
-    await Archiver.extractContentPackage(pathToSaveZipTo, folderWithUnzippedContent);
+        let qtiXml = "";
+        await FileHandler.readXml(qtiXmlFile)
+            .then((xml) => QtiCorrector.selectCorrectAnswer(xml))
+            .then((xml) => QtiCorrector.removeClassAttributes(xml))
+            .then((xml) => QtiCorrector.addXmlDeclaration(xml))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'qticomment'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'qtimetadata'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'assessmentcontrol'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'rubric'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'duration'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'presentation_material'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'assessfeedback'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'selection_ordering'))
+            .then((xml) => QtiCorrector.dropNodes(xml, 'itemmetadata'))
+            .then((xml) => QtiCorrector.removeEmptyAnswers(xml))
+            .then((xml) => QtiCorrector.unwrapContent(xml, 'flow'))
+            .then((xml) => QtiCorrector.fixWhitespace(xml))
+            .then((xml) => qtiXml = xml);
 
-    let qtiXml = "";
-    await FileHandler.readXml(qtiXmlFile)
-        .then((xml) => QtiCorrector.selectCorrectAnswer(xml))
-        .then((xml) => QtiCorrector.removeClassAttributes(xml))
-        .then((xml) => QtiCorrector.addXmlDeclaration(xml))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'qticomment'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'qtimetadata'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'assessmentcontrol'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'rubric'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'duration'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'presentation_material'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'assessfeedback'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'selection_ordering'))
-        .then((xml) => QtiCorrector.dropNodes(xml, 'itemmetadata'))
-        .then((xml) => QtiCorrector.removeEmptyAnswers(xml))
-        .then((xml) => QtiCorrector.unwrapContent(xml, 'flow'))
-        .then((xml) => QtiCorrector.fixWhitespace(xml))
-        .then((xml) => qtiXml = xml);
+        const examTitle = (await QtiCorrector.getExamTitle(qtiXml)).replace(',', '');
 
-    const examTitle = (await QtiCorrector.getExamTitle(qtiXml)).replace(',', '');
+        let manifestXml = "";
+        await FileHandler.readXml(manifestXmlFile)
+            .then((xml) => ManifestCorrector.addSchemaTag(xml))
+            .then((xml) => ManifestCorrector.addSchemaVersionTag(xml))
+            .then((xml) => ManifestCorrector.setTitle(xml, examTitle))
+            .then((xml) => ManifestCorrector.fixWhitespace(xml))
+            .then((xml) => manifestXml = xml);
 
-    let manifestXml = "";
-    await FileHandler.readXml(manifestXmlFile)
-        .then((xml) => ManifestCorrector.addSchemaTag(xml))
-        .then((xml) => ManifestCorrector.addSchemaVersionTag(xml))
-        .then((xml) => ManifestCorrector.setTitle(xml, examTitle))
-        .then((xml) => ManifestCorrector.fixWhitespace(xml))
-        .then((xml) => manifestXml = xml);
+        await FileHandler.writeXml(qtiXml, `${pathToUnzipTo}\\${examTitle}.xml`);
+        await FileHandler.deleteFile(qtiXmlFile);
+        await FileHandler.writeXml(manifestXml, manifestXmlFile);
 
-    await FileHandler.writeXml(qtiXml, `${folderWithUnzippedContent}\\${examTitle}.xml`);
-    await FileHandler.deleteFile(qtiXmlFile);
-    await FileHandler.writeXml(manifestXml, manifestXmlFile);
+        const outputFile = `${baseFolder}\\${guid}\\Brightspace-${examTitle}.zip`;
+        await Archiver.rezip(pathToUnzipTo, outputFile);
 
-    await Archiver.rezip(folderWithUnzippedContent, `c:\\Users\\mike\\Downloads\\${examTitle}.zip`);
+        await FileHandler.deleteUnzipDirectory(pathToUnzipTo);
 
-    await FileHandler.deleteUnzipDirectory(folderWithUnzippedContent);
+        console.log(`Completed processing ${assessmentId}. Files saved to ${outputFile}`);
+    }
+
+    static async start() {
+        const exams = [
+            {
+                id: 106635,
+                guid: 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b',
+            },
+            {
+                id: 106636,
+                guid: 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b',
+            },
+            {
+                id: 106638,
+                guid: 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b',
+            },
+            {
+                id: 106639,
+                guid: 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b',
+            },
+            {
+                id: 106640,
+                guid: 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b',
+            },
+            {
+                id: 106641,
+                guid: 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b',
+            },
+        ];
+
+        for (let exam of exams) {
+            await App.process(exam.id, exam.guid);
+        }
+
+        console.log('Done with batch.');
+    }
 }
 
-start(106635, 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b');
-// start(106636, 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b');
-// start(106638, 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b');
-// start(106639, 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b');
-// start(106640, 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b');
-// start(106641, 'b7d1a71a-f4c2-42d1-9642-dcea4c03622b');
+App.start()
